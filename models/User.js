9aa38f0b79b168bf6c45a0597a8fafcd35560cbd
pran17 <17pran@users.noreply.github.com>
2025-07-27 16:30:56 +0530

    user schema

--- models/User.js
+++ models/User.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['company', 'vendor'],
-    required: true,
+    required: true, 
   },
   email: {
     type: String,
@@ -20,16 +13,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ['company', 'vendor'],
+    required: true,
+  },
 });
-
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
 
 module.exports = mongoose.model('User', userSchema);
